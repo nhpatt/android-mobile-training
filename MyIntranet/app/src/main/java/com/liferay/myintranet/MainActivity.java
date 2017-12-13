5db1153330b8031a1fc35350a4c06d3c9fb81846
Javier Gamarra <nhpatt@gmail.com>
2017-12-13 17:08:55 +0100

    store state

--- MyIntranet/app/src/main/java/com/liferay/myintranet/MainActivity.java
+++ MyIntranet/app/src/main/java/com/liferay/myintranet/MainActivity.java
@@ -3,6 +3,7 @@ package com.liferay.myintranet;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,8 +11,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+	private static final String TAG = "MyIntranet";
 	private EditText emailView;
 	private EditText passwordView;
+	private boolean value;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +29,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	}
 
 	@Override
+	protected void onResume() {
+		super.onResume();
+
+		Log.e(TAG, String.valueOf(value));
+	}
+
+	@Override
 	public void onClick(View v) {
+
+		this.value = true;
 
 		if ("nhpatt@gmail.com".equals(emailView.getText().toString()) && "pass".equals(
 			passwordView.getText().toString())) {
@@ -34,5 +46,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		} else {
 			Toast.makeText(this, "Wrong user or password :(", Toast.LENGTH_LONG).show();
 		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+
+		outState.putBoolean("key", value);
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		value = savedInstanceState.getBoolean("key");
+
+		super.onRestoreInstanceState(savedInstanceState);
 	}
 }
