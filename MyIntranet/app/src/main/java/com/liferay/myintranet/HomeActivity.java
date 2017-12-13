59ff095138d691b8569077f323610dfec11be1ea
Javier Gamarra <nhpatt@gmail.com>
2017-12-13 16:49:42 +0100

    android lifecycle

--- MyIntranet/app/src/main/java/com/liferay/myintranet/HomeActivity.java
+++ MyIntranet/app/src/main/java/com/liferay/myintranet/HomeActivity.java
@@ -1,13 +1,50 @@
 package com.liferay.myintranet;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class HomeActivity extends AppCompatActivity {
+
+	private static final String TAG = "MyIntranet";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+
+		Log.e(TAG, "onCreate");
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.e(TAG, "onStart");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.e(TAG, "onResume");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.e(TAG, "onPause");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.e(TAG, "onStop");
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.e(TAG, "onDestroy");
 	}
 }
