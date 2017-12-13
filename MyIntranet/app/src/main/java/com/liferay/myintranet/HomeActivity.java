b9fa78551bc0703f262832d869410431f0a312ea
Javier Gamarra <nhpatt@gmail.com>
2017-12-13 17:08:48 +0100

    remove unneeded code

--- MyIntranet/app/src/main/java/com/liferay/myintranet/HomeActivity.java
+++ MyIntranet/app/src/main/java/com/liferay/myintranet/HomeActivity.java
@@ -14,37 +14,6 @@ public class HomeActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
-
-		Log.e(TAG, "onCreate");
 	}
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		Log.e(TAG, "onStart");
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		Log.e(TAG, "onResume");
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-		Log.e(TAG, "onPause");
-	}
-
-	@Override
-	protected void onStop() {
-		super.onStop();
-		Log.e(TAG, "onStop");
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		Log.e(TAG, "onDestroy");
-	}
 }
