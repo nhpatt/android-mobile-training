f906795a01b02f901e523e53e78d15e27e39beeb
Javier Gamarra <nhpatt@gmail.com>
2017-12-17 19:26:50 +0100

    with async tasks

--- MyIntranet/app/src/main/java/com/liferay/myintranet/WebContentActivity.java
+++ MyIntranet/app/src/main/java/com/liferay/myintranet/WebContentActivity.java
@@ -1,12 +1,15 @@
 package com.liferay.myintranet;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 import com.liferay.mobile.android.auth.basic.BasicAuthentication;
 import com.liferay.mobile.android.service.Session;
 import com.liferay.mobile.android.service.SessionImpl;
 import com.liferay.mobile.android.v7.journalarticle.JournalArticleService;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class WebContentActivity extends AppCompatActivity {
@@ -18,21 +21,35 @@ public class WebContentActivity extends AppCompatActivity {
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					BasicAuthentication basicAuthentication = new BasicAuthentication("test@liferay.com", "test");
-					Session session = new SessionImpl("http://10.0.3.2:8080", basicAuthentication);
+		new WebContentAsyncTask().execute();
+	}
 
-					JournalArticleService journalArticleService = new JournalArticleService(session);
+	class WebContentAsyncTask extends AsyncTask<Void, Void, JSONObject> {
 
-					JSONObject article = journalArticleService.getArticle(44492);
-					System.out.println(article);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+		@Override
+		protected JSONObject doInBackground(Void... voids) {
+			BasicAuthentication basicAuthentication = new BasicAuthentication("test@liferay.com", "test");
+			Session session = new SessionImpl("http://10.0.3.2:8080", basicAuthentication);
+
+			JournalArticleService journalArticleService = new JournalArticleService(session);
+
+			try {
+				return journalArticleService.getArticle(44492);
+			} catch (Exception e) {
+				return null;
 			}
-		}).start();
+		}
+
+		@Override
+		protected void onPostExecute(JSONObject article) {
+			super.onPostExecute(article);
+
+			TextView content = findViewById(R.id.web_content);
+			try {
+				content.setText(article.getString("content"));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
