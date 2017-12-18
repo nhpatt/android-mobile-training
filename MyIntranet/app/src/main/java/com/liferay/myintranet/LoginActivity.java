fca8e13c32f361242bf62fa4149c1bf6424ce3e7
Javier Gamarra <nhpatt@gmail.com>
2017-12-18 02:47:38 +0100

    listener

--- MyIntranet/app/src/main/java/com/liferay/myintranet/LoginActivity.java
+++ MyIntranet/app/src/main/java/com/liferay/myintranet/LoginActivity.java
@@ -1,13 +1,30 @@
 package com.liferay.myintranet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import com.liferay.mobile.screens.auth.login.LoginListener;
+import com.liferay.mobile.screens.auth.login.LoginScreenlet;
+import com.liferay.mobile.screens.context.User;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		LoginScreenlet loginScreenlet = findViewById(R.id.login);
+		loginScreenlet.setListener(this);
+	}
+
+	@Override
+	public void onLoginSuccess(User user) {
+		Toast.makeText(this, user.getFirstName(), Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onLoginFailure(Exception e) {
+		Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
 	}
 }
