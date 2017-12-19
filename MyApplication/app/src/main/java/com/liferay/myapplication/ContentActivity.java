9736ec15ffee6a0672016c22bdf038f3754514d1
Javier Gamarra <nhpatt@gmail.com>
2017-12-19 10:20:59 +0100

    push screens

--- MyApplication/app/src/main/java/com/liferay/myapplication/ContentActivity.java
+++ MyApplication/app/src/main/java/com/liferay/myapplication/ContentActivity.java
@@ -1,38 +1,19 @@
 package com.liferay.myapplication;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import com.liferay.mobile.push.Push;
+import com.liferay.mobile.android.service.Session;
 import com.liferay.mobile.screens.context.SessionContext;
+import com.liferay.mobile.screens.push.PushScreensActivity;
 import com.liferay.mobile.screens.webcontent.display.WebContentDisplayScreenlet;
 import org.json.JSONObject;
 
-public class ContentActivity extends AppCompatActivity {
+public class ContentActivity extends PushScreensActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_content);
-
-		//try {
-		//	Push.with(SessionContext.createSessionFromCurrentSession())
-		//		.withPortalVersion(70)
-		//		.register(this, "176310287574");
-		//} catch (Exception e) {
-		//	e.printStackTrace();
-		//}
-
-		Push.with(SessionContext.createSessionFromCurrentSession()).onPushNotification(
-			new Push.OnPushNotification() {
-
-				@Override
-				public void onPushNotification(JSONObject jsonObject) {
-					Log.e("ajksdjkasdf", jsonObject.toString());
-				}
-			}
-		);
-
 
 		WebContentDisplayScreenlet screenlet = (WebContentDisplayScreenlet) findViewById(R.id.web_content_display);
 		screenlet.setArticleId("30902");
@@ -40,5 +21,25 @@ public class ContentActivity extends AppCompatActivity {
 
 		UserProfileScreenlet viewById = (UserProfileScreenlet) findViewById(R.id.my_user_profile);
 		viewById.performUserAction();
+	}
+
+	@Override
+	protected Session getDefaultSession() {
+		return SessionContext.createSessionFromCurrentSession();
+	}
+
+	@Override
+	protected void onPushNotificationReceived(JSONObject jsonObject) {
+		Log.e("ajksdjkasdf", jsonObject.toString());
+	}
+
+	@Override
+	protected void onErrorRegisteringPush(String message, Exception e) {
+		Log.e("ajksdjkasdf", e.getMessage());
+	}
+
+	@Override
+	protected String getSenderId() {
+		return "176310287574";
 	}
 }
